509653154a23cd13db274ef8575fc0e99f0253e0
Jayphen <jayphen@gmail.com>
2011-09-26 15:11:55 +1000

    Refactored to use bindings, removed password support

--- js/jquery.placeholder.js
+++ js/jquery.placeholder.js
@@ -1,81 +1,45 @@
 (function($) {
+  /**
+   * Spoofs placeholders in browsers that don't support them (eg Firefox 3)
+   * 
+   * Copyright 2011 Dan Bentley
+   * Licensed under the Apache License 2.0
+   *
+   * Author: Dan Bentley [github.com/danbentley]
+   * Refactored by: Jayphen [github.com/jayphen]
+   */
 
-	/**
-	 * Spoofs placeholders in browsers that don't support them (eg Firefox 3)
-	 * 
-	 * Copyright 2011 Dan Bentley
-	 * Licensed under the Apache License 2.0
-	 *
-	 * Author: Dan Bentley [github.com/danbentley]
-	 */
+  // Return if native support is available.
+  if ("placeholder" in document.createElement("input")) return;
 
-	// Return if native support is available.
-	if ("placeholder" in document.createElement("input")) return;
+  $("input[placeholder]").each(function() {
+    var self = $(this),
+      holder = self.attr('placeholder');
 
-	$(document).ready(function(){
-		$(':input[placeholder]').not(':password').each(function() {
-			setupPlaceholder($(this));
-		});
+    self.bind('init.placeholder', function() {
+      self.val() === '' ? self.val(holder).addClass('placeholder') : self.data('changed', true).removeClass('placeholder');
+    })
 
-		$(':password[placeholder]').each(function() {
-			setupPasswords($(this));
-		});
-	   
-		$('form').submit(function(e) {
-			clearPlaceholdersBeforeSubmit($(this));
-		});
-	});
+    .bind('submitCheck.placeholder', function() {
+      if (self.data('changed')) return;
+      if (self.val() === holder) self.val('');
+    })
 
-	function setupPlaceholder(input) {
+    .bind('focus.placeholder', function() {
+      self.removeClass('placeholder').trigger('submitCheck.placeholder');
+    })
 
-		var placeholderText = input.attr('placeholder');
+    .bind('blur.placeholder', function() {
+      self.trigger('init.placeholder');
+    })
 
-		setPlaceholderOrFlagChanged(input, placeholderText);
-		input.focus(function(e) {
-			if (input.data('changed') === true) return;
-			if (input.val() === placeholderText) input.val('');
-		}).blur(function(e) {
-			if (input.val() === '') input.val(placeholderText); 
-		}).change(function(e) {
-			input.data('changed', input.val() !== '');
-		});
-	}
+    .bind('change.placeholder', function() {
+      self.data('changed', self.val() !== '');
+    });
 
-	function setPlaceholderOrFlagChanged(input, text) {
-		(input.val() === '') ? input.val(text) : input.data('changed', true);
-	}
+  }).trigger('init.placeholder');
 
-	function setupPasswords(input) {
-		var passwordPlaceholder = createPasswordPlaceholder(input);
-		input.after(passwordPlaceholder);
-
-		(input.val() === '') ? input.hide() : passwordPlaceholder.hide();
-
-		$(input).blur(function(e) {
-			if (input.val() !== '') return;
-			input.hide();
-			passwordPlaceholder.show();
-		});
-			
-		$(passwordPlaceholder).focus(function(e) {
-			input.show().focus();
-			passwordPlaceholder.hide();
-		});
-	}
-
-	function createPasswordPlaceholder(input) {
-		return $('<input>').attr({
-			placeholder: input.attr('placeholder'),
-			value: input.attr('placeholder'),
-			id: input.attr('id'),
-			readonly: true
-		}).addClass(input.attr('class'));
-	}
-
-	function clearPlaceholdersBeforeSubmit(form) {
-		form.find(':input[placeholder]').each(function() {
-			if ($(this).data('changed') === true) return;
-			if ($(this).val() === $(this).attr('placeholder')) $(this).val('');
-		});
-	}
+  $('form').has("input[placeholder]").bind('submit', function(e) {
+    $(this).find("input[placeholder]").trigger('submitCheck.placeholder');
+  });
 })(jQuery);
